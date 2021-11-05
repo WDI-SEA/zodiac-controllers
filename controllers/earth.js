e7a2028b0983efbbde2abd59045c83436364f574
LindZCoding <Lindzgaming@gmail.com>
2021-11-04 18:16:57 -0700

    added images controllers views links

---
+++ controllers/earth.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/capricorn", (req, res)=> {
+   
+    res.render("earth/capricorn.ejs")
+})
+
+router.get("/taurus", (req, res)=> {
+   
+    res.render("earth/taurus.ejs")
+})
+
+router.get("/virgo", (req, res)=> {
+   
+    res.render("earth/virgo.ejs")
+})
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use("/air", require("./controllers/air.js"))
+app.use("/earth", require("./controllers/earth.js"))
+app.use("/fire", require("./controllers/fire.js"))
+app.use("/water", require("./controllers/water.js"))
+
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/water', (req, res) => {
+    const waterSigns = ['Pisces', 'Cancer', 'Scorpio']
+    const water = ['private', 'mysterious', 'pyshic', 'charming', 'emotional', 'sensitive']
+    res.render('water.ejs', {waterSigns: waterSigns, water: water})
+})
+
+app.get('/air', (req, res) => {
+    const airSigns = ['Aquarius', 'Gemini', 'Libra']
+    const air = ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']
+    res.render('air.ejs', {airSigns: airSigns, air: air})
+})
+
+app.get('/fire', (req, res) => {
+    const fireSigns = ['Aries', 'Leo', 'Sagittarius']
+    const fire = ['passionate', 'strong emotions', 'tempermental', 'energetic', 'accomplished', 'interesting']
+    res.render('fire.ejs', {fireSigns: fireSigns, fire: fire})
+})
+
+app.get('/earth', (req, res) => {
+    const earthSigns = ['Taurus', 'Virgo', 'Capricorn']
+    const earth = ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']
+    res.render('earth.ejs', {earthSigns: earthSigns, earth: earth})
+})
+
+app.listen(8000, () => {
+    console.log('connected')
+})
