1f40615050e8399163e871ac3c0d028559a46a0e
romanprotoliuk <romanprotoliuk@gmail.com>
2022-02-18 10:10:47 -0800

    More templates

--- index.js
+++ index.js
@@ -6,32 +6,52 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayout);
 
+app.use((req, res, next) => {
+	console.log('hello from middleware');
+
+	next();
+});
+
 app.get('/', (req, res) => {
 	res.render('index.ejs');
 });
 
 app.get('/water', (req, res) => {
-	res.render('water.ejs', { traits: [ 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive' ] });
+	res.render('water.ejs', {
+		element: 'Water',
+		traits: [ 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive' ],
+		signs: [ 'Pisces', 'Cancer', 'Scorpio' ]
+	});
 });
 
 app.get('/air', (req, res) => {
 	res.render('air.ejs', {
-		traits: [ 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked' ]
+		element: 'Air',
+		traits: [ 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked' ],
+		signs: [ 'Aquarious', 'Gemini', 'Libra' ]
 	});
 });
 
 app.get('/fire', (req, res) => {
 	res.render('fire.ejs', {
-		traits: [ 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting' ]
+		element: 'Fire',
+		traits: [ 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting' ],
+		signs: [ 'Aries', 'Leo', 'Sagittarious' ]
 	});
 });
 
 app.get('/earth', (req, res) => {
 	res.render('earth.ejs', {
-		traits: [ 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable' ]
+		element: 'Earth',
+		traits: [ 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable' ],
+		signs: [ 'Taurus', 'Virgo', 'Capricorn' ]
 	});
 });
 
-app.listen(3000, () => {
+app.use((req, res) => {
+	res.send('404 error, page not found');
+});
+
+app.listen(2000, () => {
 	console.log('Nodemon is running');
 });
