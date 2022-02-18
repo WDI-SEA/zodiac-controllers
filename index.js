d4d23c6f777bfad6a098ba95721e4c42a57a671e
vijaylpersad <vijaylpersad@utexas.edu>
2022-02-18 12:00:30 -0600

    review pt 1

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 
 
-const express = require('express')
-const app = express()
-//import ejs layouts
+const express = require('express') //import express
 const ejsLayouts = require('express-ejs-layouts')
 
+const app = express() //create instance of express . app is industry standard 
+//import ejs layouts
+PORT = 8000
 
 //middleware
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 
-//middleware
-app.set('view engine', 'ejs')
-app.use(ejsLayouts)
 
 app.get('/', (req,res)=>{
-    //res.send('hello')
+    //res.send('hello') could also put html elements inside res.send i.e. <p>
     res.render('index.ejs')
 })
 
 app.get('/water', (req,res)=>{
-    res.render('water.ejs', {waterArr: ['private','mysterious','psychic','charming','emotional','sensitive']})
+    res.render('water.ejs', {signs: ['pisces', 'cancer', 'scorpio'], waterArr: ['private','mysterious','psychic','charming','emotional','sensitive']})
 })
 
 app.get('/air', (req,res)=>{
-    res.render('air.ejs', {airArr: ['movement','creativity','action','adventure','exciting','easily provoked']})
+    res.render('air.ejs', {signs: ['aquarius', 'gemini', 'libra'], airArr: ['movement','creativity','action','adventure','exciting','easily provoked']})
 })
 
 app.get('/fire', (req,res)=>{
-    res.render('fire.ejs', {fireArr: ['passionate','strong emotions','temperamental','energetic','accomplished','interesting']})
+    res.render('fire.ejs', {signs: ['aries', 'leo', 'sagittarius'], fireArr: ['passionate','strong emotions','temperamental','energetic','accomplished','interesting']})
 })
 
 app.get('/earth', (req,res)=>{
-    res.render('earth.ejs', {earthArr: ['grounded','helpful','practical','realistic','materialistic','dependable']})
+    res.render('earth.ejs', {signs: ['taurus', 'virgo', 'capricorn'], earthArr: ['grounded','helpful','practical','realistic','materialistic','dependable']})
 })
 
-app.listen(8000, ()=>{
-    console.log('heheh')
+app.listen(PORT, err =>{
+    console.log(`Listening to the sounds of ${PORT}`)
+    if (err) console.log(err)
 })
 
