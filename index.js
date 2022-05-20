6d1399c3cc072ede0cb4dba6b9416b61e4537c96
gracenarez333 <gracemnarez@gmail.com>
2022-05-20 09:49:47 -0700

    updates signs

--- index.js
+++ index.js
@@ -17,26 +17,30 @@ app.get('/', (req, res) => {
 
 // water
 app.get('/water', (req, res) => {
+    const waterSigns = ['PICSES', 'CANCER', 'SCORPIO']
     const waterTraits = ['private', 'mysterious', 'pyschic', 'charming', 'emotional', 'sensitive']
-    res.render('water.ejs', {waterTraits})
+    res.render('water.ejs', {waterSigns, waterTraits})
 })
 
 // air
 app.get('/air', (req, res) => {
+    const airSigns = ['AQUARIUS', 'GEMINI', 'LIBRA']
     const airTraits = ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']
-    res.render('air.ejs', {airTraits})
+    res.render('air.ejs', {airSigns, airTraits})
 })
 
 // fire
 app.get('/fire', (req, res) => {
+    const fireSigns = ['ARIES', 'LE0', 'SAGITARIUS']
     const fireTraits = ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']
-    res.render('fire.ejs', {fireTraits})
+    res.render('fire.ejs', {fireSigns, fireTraits})
 })
 
 // earth
 app.get('/earth', (req, res) => {
+    const earthSigns = ['TAURUS', 'VIRGO', 'CAPRICORN']
     const earthTraits = ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']
-    res.render('earth.ejs', {earthTraits})
+    res.render('earth.ejs', {earthSigns, earthTraits})
 })
 
 
