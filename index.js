61a1c81cb43ce8cb80ad2ba179134cb18e5d9c8b
marzo20 <myjune91@gmail.com>
2022-05-20 10:10:01 -0700

    class solution.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // set up npms
 const express = require('express')
-const app = express()
-const PORT = 2002
 const ejsLayouts = require('express-ejs-layouts')
-app.set('view engine', 'ejs')
 
+// app config
+const app = express()
+app.set('view engine', 'ejs')
+const PORT = 2002
+
+// middlewares
 app.use(ejsLayouts)
 
 
@@ -13,24 +16,39 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 app.get('/water', (req, res)=>{
-    const waters = ['PISCES', 'CANCER', 'SCORPIO']
-    res.render('water.ejs', {waters})
+    res.render('water.ejs', {
+        element: 'Water',
+        signs: ['PISCES', 'CANCER', 'SCORPIO'],
+        traits: ['private', 'mysterious', 'psychic', 'charming', 'emo']
+    })
 })
 app.get('/air', (req, res)=>{
-    const airs = ['AQUARIUS', 'GEMINI', 'LIBRA']
-    res.render('air.ejs', {airs})
+    res.render('air.ejs', {
+        element: 'Air',
+        signs: ['AQUARIUS', 'GEMINI', 'LIBRA'],
+        traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']
+    })
 })
 app.get('/fire', (req, res)=>{
-    const fires = ['ARIES', 'LEO', 'SAGITTARIUS']
-    res.render('fire.ejs', {fires})
+    res.render('fire.ejs', {
+        element: 'Fires',
+        signs: ['ARIES', 'LEO', 'SAGITTARIUS'],
+        traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']
+    })
 })
 app.get('/earth', (req, res)=>{
     const earths = ['TAURUS', 'VIRGO', 'CAPRICORN']
-    res.render('earth.ejs', {earths})
+    res.render('earth.ejs', {
+        element: 'earth',
+        signs: ['TAURUS', 'VIRGO', 'CAPRICORN'],
+        traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']
+    })
 })
 app.get('/img', (req, res) => {
     res.sendFile('/Users/wonjunejung/GA/sei/unit2/zodiac-controllers/zodiac.jpeg')
 }) 
+
+// listening on a port
 app.listen(PORT, () => {
     console.log(PORT, 'linked')
 })
