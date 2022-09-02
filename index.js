71f7654c1bcf531940f98a9c712cb0e11364a2b8
brandon-w1205 <bwu1205@gmail.com>
2022-09-01 21:01:45 -0700

    adds element links to load symbols

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 const waterTraits = ['PISCES', 'CANCER', 'SCORPIO', 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive'];
 const airTraits = ['AQUARIUS', 'GEMINI', 'LIBRA', 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked'];
 const fireTraits = ['ARIES', 'LEO', 'SAGITTARIUS', 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting'];
 const earthTraits = ['TAURUS', 'VIRGO', 'CAPRICORN', 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable'];
 
+
 app.get('/', (req, res) => {
     // res.send('This is the Home Page')
     res.render('index')
 })
+
 
 app.get('/water', (req, res) => {
     // res.send('This is the water page')
