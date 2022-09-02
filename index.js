eae526e31a0c8c6cf2e5826238daedd325f9103e
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-01 17:18:18 -0700

    add all res statements

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const app = express()
 const PORT = 8000
 
 //set routes
+app.get('/', (req,res) =>{
+    res.send("hola")
+})
+app.get('/water', (req,res) =>{
+    res.send('agua')
+})
 
+app.get('/air', (req,res) =>{
+    res.send('aire')
+})
 
+app.get('/fire', (req,res) =>{
+    res.send("fuego")
+})
+
+app.get('/earth', (req,res) =>{
+    res.send('mundo')
+})
 //set port listener
 
 app.listen(PORT, () =>{
