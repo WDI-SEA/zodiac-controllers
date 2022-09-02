9c91225670ee2d295252e6369219ce4dc14cb7f9
elek8871 <elekmichelle@gmail.com>
2022-09-01 21:19:34 -0600

    stubs out elements

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const PORT = 8000
 // // configure for EJS
 app.set('view engine', 'ejs')
 
+// need navi links
+app.get('/', (req, res)=>{
+    res.send("navi links here")
+})
+// water
+app.get('/water', (req, res) => {
+    res.send("water")
+})
+// air
+app.get('/air', (req,res)=>{
+    res.send("air")
+})
+//fire
+app.get('/fire', (req, res)=>{
+    res.send("fire")
+})
+//earth
+app.get('/earth', (req, res)=>{
+    res.send("earth")
+})
+
+
+
+
+
  app.listen(PORT, ()=> {
     console.log(`you are live on channel ${8000}`)
  })
