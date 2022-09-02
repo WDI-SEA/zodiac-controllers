f943db2c4bacb7eb99e7132304c5a3c731a5520b
average-alien <allentang113@gmail.com>
2022-09-01 22:43:38 -0700

    sets up index.ejs - adds element objects to index.js

--- index.js
+++ index.js
@@ -5,12 +5,31 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+// VARIABLES
+const air = {
+    signs: ['Aquarius', 'Gemini', 'Libra'],
+    traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']
+}
+const water = {
+    signs: ['Pisces', 'Cancer', 'Scorpio'],
+    traits: ['private', 'mysterious', 'pyshic', 'charming', 'emotional', 'sensitive']
+}
+const fire = {
+    signs: ['Aries', 'Leo', 'Sagittarius'],
+    traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']
+}
+const earth = {
+    signs: ['Taurus', 'Virgo', 'Capricorn'],
+    traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']
+}
+
 // configure app to use ejs
 app.set('view engine', 'ejs')
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('home page')
+    // res.send('home page')
+    res.render('index.ejs')
 })
 
 app.get('/water', (req, res) => {
