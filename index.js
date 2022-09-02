5c48fe8b38768a3b66cda725531fe2975b3ef9ac
Agrate071818 <zionagrate@gmail.com>
2022-09-01 23:58:48 -0400

    Links added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const wTrait = ['private','mysterious','pyshic','charming','emotional','sensitive']
 const PORT = 8000
 
 app.set('view engine', 'ejs');
@@ -9,29 +9,37 @@ app.get('/', (req, res) => {
     res.render('homepage')
 });
 
+const aTrait = ['movement','creativity','action','adventure','exciting','easily provoked']
 app.get("/air", (request, response) => {
 
-    response.render('air')
+    response.render('air', {traits:aTrait})
 
 })
 
-app.get("/earth", (request, response) => {
-
-    response.render('earth')
-
-})
-
-app.get("/fire", (request, response) => {
-
-    response.render('fire')
-
-})
-
+// const wTrait = ['private','mysterious','pyshic','charming','emotional','sensitive']
 app.get("/water", (request, response) => {
 
-    response.render('water')
+    response.render('water', {traits: wTrait})
 
 })
+
+const fTrait = ['passionate','strong emotions','temperamental','energetic','accomplished','interesting']
+app.get("/fire", (request, response) => {
+
+    response.render('fire', {traits:fTrait})
+
+})
+
+const eTrait = ['grounded','helpful','practical','realistic','materialistic','dependable']
+app.get("/earth", (request, response) => {
+
+    response.render('earth', {traits:eTrait})
+
+})
+
+
+
+
 
 app.listen(PORT, (req,res) => {
     console.log(`Port ${PORT} is working`)
