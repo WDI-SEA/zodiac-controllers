de0bba7b7970e935fca04bf05b70a6eb584939f0
Moodybleu <meganbenn27@icloud.com>
2022-09-02 08:38:59 -0700

    Adds remaining routes for index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/.', (req, res) => {
     // console.log('Welcome nighthawks, we have been expecting you🎩')
     res.send('Beam me up scotty! 🚀')
 })
+app.get('/.', (req, res) => {
+    // console.log('Welcome nighthawks, we have been expecting you🎩')
+    res.send('Luke! i AM your Father')
+})
+app.get('/.', (req, res) => {
+    // console.log('Welcome nighthawks, we have been expecting you🎩')
+    res.send('NOOOOOOOOOO!!')
+})
+app.get('/.', (req, res) => {
+    // console.log('Welcome nighthawks, we have been expecting you🎩')
+    res.send('1.21 GIGAWATTS!!')
+})
 // tell express to listen on a port for incoming http requests
 app.listen(PORT, () => {
     console.log(`Welcome to the nest on port ${PORT} nighthawk 🎩`)
