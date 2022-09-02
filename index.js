55515c1f4fed743640c295f0b81106c287ba68b2
brandon-w1205 <bwu1205@gmail.com>
2022-09-01 20:05:49 -0700

    adds trait arrays and adds them as objects

--- index.js
+++ index.js
@@ -5,25 +5,34 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs')
 
+const waterTraits = ['PISCES', 'CANCER', 'SCORPIO', 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive'];
+const airTraits = ['AQUARIUS', 'GEMINI', 'LIBRA', 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked'];
+const fireTraits = ['ARIES', 'LEO', 'SAGITTARIUS', 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting'];
+const earthTraits = ['TAURUS', 'VIRGO', 'CAPRICORN', 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable'];
+
 app.get('/', (req, res) => {
     // res.send('This is the Home Page')
     res.render('index')
 })
+
 app.get('/water', (req, res) => {
     // res.send('This is the water page')
-    res.render('water')
+    res.render('water', {wTraits: waterTraits})
 })
+
 app.get('/air', (req, res) => {
     // res.send('This is the air page')
-    res.render('air')
+    res.render('air', {aTraits: airTraits})
 })
+
 app.get('/fire', (req, res) => {
     // res.send('This is the fire page')
-    res.render('fire')
+    res.render('fire', {fTraits: fireTraits})
 })
+
 app.get('/earth', (req, res) => {
     // res.send('This is the earth page')
-    res.render('earth')
+    res.render('earth', {eTraits: earthTraits})
 })
 
 app.listen(PORT, () => {
