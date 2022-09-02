d0fc36ab84f672d749c68917c70cc9ed663ca933
tylerchan33 <tylerchan33@gmail.com>
2022-09-01 18:32:09 -0700

    completed part 1

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const app = express()
+
+const PORT = 3000
+
+const waterSigns = ["Pisces", "Cancer", "Scorpio"]
+const airSigns = ["Aquarius", "Gemini", "Libra"]
+const fireSigns = ["Aries", "Leo", "Sagittarius"]
+const earthSigns = ["Taurus", "Virgo", "Capricorn"]
+const waterTraits = ["private", "mysterious", "psychic", "charming", "emotional", "sensitive"]
+const airTraits = ["movement", "creativity", "action", "adventure", "exciting", "easily provoked"]
+const fireTraits = ["passionate", "strong emotions", "temperamental", "energetic", "accomplished", "interesting"]
+const earthTraits = ["grounded", "helpful", "practical", "realistic", "materialistic", "dependable"]
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/water", (req, res) => {
+    res.render("water", {signs: waterSigns, water: waterTraits})
+})
+
+app.get("/air", (req, res) => {
+    res.render("air", {signs: airSigns, air: airTraits})
+})
+
+app.get("/fire", (req, res) => {
+    res.render("fire", {signs: fireSigns, fire: fireTraits})
+})
+
+app.get("/earth", (req, res) => {
+    res.render("earth", {signs: earthSigns, earth: earthTraits})
+})
+
+
+app.listen(3000, () => {
+    console.log("server is up")
+})
