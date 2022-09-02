896389f0c6de28a261ab88b683f727082ff257b7
tatebonham <tatebonham@gmail.com>
2022-09-02 10:31:26 -0700

    Updated hw to be more dynamic

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ app.set('view engine', 'ejs')
 
 
 app.get('/air', (req, res) =>{
-    res.render('air', {signs: ['Aquarius', ' Gemini', ' Libra'], traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']})
+    res.render('show', {type: 'Air', signs: ['Aquarius', ' Gemini', ' Libra'], traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']})
 })
 app.get('/water', (req, res) =>{
-    res.render('water', {signs: ['Pisces',  'Cancer', ' Scorpio'], traits: ['private', 'mysterious', 'pyshic', 'charming', 'emotional', 'sensitive']})
+    res.render('show', {type: 'Water', signs: ['Pisces',  'Cancer', ' Scorpio'], traits: ['private', 'mysterious', 'pyshic', 'charming', 'emotional', 'sensitive']})
 })
 app.get('/fire', (req, res) =>{
-    res.render('fire', {signs: ['Aries', ' Leo', ' Sagittarius'], traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']})
+    res.render('show', {type: 'Fire', signs: ['Aries', ' Leo', ' Sagittarius'], traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']})
 })
 app.get('/earth', (req, res) =>{
-    res.render('earth', {signs: ['Taurus', ' Virgo', ' Capricorn'], traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']})
+    res.render('show', {type: 'Earth', signs: ['Taurus', ' Virgo', ' Capricorn'], traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']})
 })
 app.get('/', (req, res) =>{
     res.render('index')
