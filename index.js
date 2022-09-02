e55ea06ab82b1026b6f343ffd7a8971a229425d4
brandon-w1205 <bwu1205@gmail.com>
2022-09-02 10:11:04 -0700

    adds show and changes array to objects for learning purposes

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs');
 
-const waterTraits = ['PISCES', 'CANCER', 'SCORPIO', 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive'];
-const airTraits = ['AQUARIUS', 'GEMINI', 'LIBRA', 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked'];
-const fireTraits = ['ARIES', 'LEO', 'SAGITTARIUS', 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting'];
-const earthTraits = ['TAURUS', 'VIRGO', 'CAPRICORN', 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable'];
+// const waterTraits = ['PISCES', 'CANCER', 'SCORPIO', 'private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive'];
+// const airTraits = ['AQUARIUS', 'GEMINI', 'LIBRA', 'movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked'];
+// const fireTraits = ['ARIES', 'LEO', 'SAGITTARIUS', 'passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting'];
+// const earthTraits = ['TAURUS', 'VIRGO', 'CAPRICORN', 'grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable'];
+
+const elements = {
+    water: {
+        name: "WATER",
+        signs: ['PISCES', 'CANCER', 'SCORPIO'],
+        traits: ['private', 'mysterious', 'psychic', 'charming', 'emotional', 'sensitive']
+    },
+    air: {
+        name: "AIR",
+        signs: ['AQUARIUS', 'GEMINI', 'LIBRA'],
+        traits: ['movement', 'creativity', 'action', 'adventure', 'exciting', 'easily provoked']
+    },
+    fire: {
+        name: "FIRE",
+        signs: ['ARIES', 'LEO', 'SAGITTARIUS'],
+        traits: ['passionate', 'strong emotions', 'temperamental', 'energetic', 'accomplished', 'interesting']
+    },
+    earth: {
+        name: "EARTH",
+        signs: ['TAURUS', 'VIRGO', 'CAPRICORN'],
+        traits: ['grounded', 'helpful', 'practical', 'realistic', 'materialistic', 'dependable']
+    }
+}
 
 
 app.get('/', (req, res) => {
@@ -19,22 +42,44 @@ app.get('/', (req, res) => {
 
 app.get('/water', (req, res) => {
     // res.send('This is the water page')
-    res.render('water', {wTraits: waterTraits})
+
+    // For the array version
+    // res.render('water', {wTraits: waterTraits})
+
+    // DATA CHECK
+    // res.json(elements.water)
+
+    res.render('show', {element: elements.water})
 })
 
 app.get('/air', (req, res) => {
     // res.send('This is the air page')
-    res.render('air', {aTraits: airTraits})
+
+    // For the array version
+    // res.render('air', {aTraits: airTraits})
+
+    // DATA CHECK
+    // res.json(elements.air)
+
+    res.render('show', {element: elements.air})
 })
 
 app.get('/fire', (req, res) => {
     // res.send('This is the fire page')
-    res.render('fire', {fTraits: fireTraits})
+
+    // For the array version
+    // res.render('fire', {fTraits: fireTraits})
+
+    res.render('show', {element: elements.fire})
 })
 
 app.get('/earth', (req, res) => {
     // res.send('This is the earth page')
-    res.render('earth', {eTraits: earthTraits})
+
+    // For the array version
+    // res.render('earth', {eTraits: earthTraits})
+
+    res.render('show', {element: elements.earth})
 })
 
 app.listen(PORT, () => {
