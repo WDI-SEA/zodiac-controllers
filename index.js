54b9ba45c78c562de46f061e30b8873d6f87182a
syorn96 <syorn2014@gmail.com>
2022-09-01 18:07:43 -0700

    creates views for elements

---
+++ index.js
@@ -0,0 +1,15 @@
+// IMPORT required packages
+const express = require('express')
+
+// CREATE instance of express app
+const app = express();
+const PORT = 8000
+// DEFINE express routes
+app.get('/', (req, res) => {
+    res.
+})
+
+// tell express to LISTEN
+app.listen(PORT, () =>{
+    console.log(`The stars are aligned in your favor... on port ${PORT}`)
+})
