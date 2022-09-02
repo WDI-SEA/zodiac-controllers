ee45d4c40c0c673fe085b98eab26d7a3403f7aaa
devin-lynch <devin.l.lynch@gmail.com>
2022-09-01 20:31:51 -0500

    Set up links for routes

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const express = require('express')
 const app = express()
-
+const ejsLayouts = require('express-ejs-layouts')
 const PORT = 3000
 
 // CONFIGURE APP TO USE EJS
-// app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 app.get('/', (req, res) => {
-    res.send(`This is the home page!`)
+    // res.send(`This is the home page!`)
+    console.log(`This is the index page!`)
+    res.render('index')
 })
 
 app.get('/water', (req, res) => {
-    res.send(`This is the water page!`)
+    // res.send(`This is the water page!`)
+    console.log(`This is the water page!`)
+    res.render('water')
 })
 
 app.get('/air', (req, res) => {
-    res.send(`This is the air page!`)
+    // res.send(`This is the air page!`)
+    console.log(`This is the air page!`)
+    res.render('air')
 })
 
 app.get('/fire', (req, res) => {
-    res.send(`This is the fire page!`)
+    // res.send(`This is the fire page!`)
+    console.log(`This is the fire page!`)
+    res.render('fire')
 })
 
 app.get('/earth', (req, res) => {
-    res.send(`This is the earth page!`)
+    // res.send(`This is the earth page!`)
+    console.log(`This is the earth page!`)
+    res.render('earth')
 })
 
 
