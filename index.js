55ca2b944cfd60b80081735fd285bc08b09157e7
syorn96 <syorn2014@gmail.com>
2022-09-01 18:44:09 -0700

    adds water traits w/homepage link

--- index.js
+++ index.js
@@ -13,19 +13,28 @@ const arrEarthsigns = ['TAURUS','VIRGO','CAPRICORN']
 const app = express();
 const PORT = 8000
 // DEFINE express routes
+
+// HOMEPAGE
 app.get('/', (req, res) => {
     res.render(`layout.ejs`)
-    res.render('index.ejs')
 })
+
+// WATER PAGE
 app.get('/water', (req, res) => {
     res.render(`water.ejs`)
 })
+
+// AIR PAGE
 app.get('/air', (req, res) => {
     res.render(`air.ejs`)
 })
+
+// FIRE PAGE
 app.get('/fire', (req, res) => {
     res.render(`fire.ejs`)
 })
+
+// EARTH PAGE
 app.get('/earth', (req, res) => {
     res.render(`earth.ejs`)
 })
