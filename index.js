fa84f651cd79cec0320771a07cec8091d896eaf2
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-01 21:02:07 -0700

    mvp complete

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const app = express()
-
+const PORT = 3000
 app.set('view engine', 'ejs')
 
 app.listen(PORT, ()=> {
     console.log(`intruder in sector ${PORT}`)
 })
 
-const fireThings = []
-const waterThings = []
-const airThings = []
-const earthThings = []
+const fireSigns = ["Aries", "Leo", "Sagittarius"]
+const fireThings = ['Passionate', 'Strong Emotions', 'Tempermental', 'Energetic', 'Accomplished', 'Interesting']
+const waterSigns = ["Pisces", "Cancer", "Scorpio"]
+const waterThings = ['Private', 'Mysterious', 'Psychic', 'Charming', 'Emotional', 'Sensitive']
+const airSigns = ["Aquarius", "Gemini", "Libra"]
+const airThings = ['Movement', 'Creativity', 'Action', 'Adventure', 'Exciting', 'Easily Provoked']
+const earthSigns = ["Taurus", "Virgo", "Capricorn"]
+const earthThings = ['Grounded', 'Helpful', 'Practical', 'Realistic', 'Materialistic', 'Dependable']
 
 
 app.get('/', (req, res) => {
-    res.render()
+    res.render('index')
 })
+app.get('/water', (req, res) => {
+    res.render('water', {waterThings, waterSigns})
+}) 
+app.get('/air', (req, res) => {
+    res.render('air', {airThings, airSigns})
+}) 
+app.get('/fire', (req, res) => {
+    res.render('fire', {fireThings, fireSigns})
+}) 
+app.get('/earth', (req, res) => {
+    res.render('earth', {earthThings, earthSigns})
+}) 
