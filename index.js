98a07aedd4d85176cde9e9488f8d5a64bebf0a92
brandon-w1205 <bwu1205@gmail.com>
2022-09-01 18:12:45 -0700

    adds layout file and creates boilerplate along with linking to other ejs files

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     // res.send('This is the Home Page')
+    res.render('index')
+})
+app.get('/water', (req, res) => {
+    // res.send('This is the water page')
+    res.render('water')
+})
+app.get('/air', (req, res) => {
+    // res.send('This is the air page')
+    res.render('air')
+})
+app.get('/fire', (req, res) => {
+    // res.send('This is the fire page')
+    res.render('fire')
+})
+app.get('/earth', (req, res) => {
+    // res.send('This is the earth page')
+    res.render('earth')
 })
 
 app.listen(PORT, () => {
