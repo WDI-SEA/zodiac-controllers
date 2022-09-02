3321815d3096fa0dd059da43bd28cee5cdb1b371
syorn96 <syorn2014@gmail.com>
2022-09-01 18:35:36 -0700

    separates links into index.html

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // IMPORT required packages
 const express = require('express')
 
+
+// Element characteristics
+
+const arrWatersigns = ['PISCES','CANCER','SCORPIO']
+const arrAirsigns = ['AQUARIUS','GEMINI','LIBA']
+const arrFiresigns = ['ARIES','LEO','SAGITTARIUS']
+const arrEarthsigns = ['TAURUS','VIRGO','CAPRICORN']
+
 // CREATE instance of express app
 const app = express();
 const PORT = 8000
 // DEFINE express routes
 app.get('/', (req, res) => {
     res.render(`layout.ejs`)
+    res.render('index.ejs')
 })
 app.get('/water', (req, res) => {
     res.render(`water.ejs`)
