92cb81956c901bfa3e5db2013f4d82427e1af7a6
Anwaar <anwaaralsyouf@gmail.com>
2022-10-15 01:19:04 +0300

    solved

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const ejsLayouts = require('express-ejs-layouts')
 
 app.set('view engine', 'ejs')
+app.use(ejsLayouts);
 
+const PORT = 3000
 
 // Routs
-
 app.get('/', (req,res)=>{
-    res.render('HOME')
-})
-
-app.get('/water', (req,res)=>{
-    res.render('WATER')
+    // res.send("Hello This is the Home page") //For Testing
+    res.render('index.ejs')
 })
 
 app.get('/air', (req,res)=>{
-    res.render('AIR')
+    res.render('air.ejs', {airTraits: ['Movement', 'Creativity', 'Action', 'Adventure', 'Exciting', 'Easily Provocked']})
+})
+
+app.get('/water', (req,res)=>{
+    res.render('water.ejs', {waterTraits: ['Private', 'Mysterious', 'Pychic', 'Charming', 'Emotional', 'Sensitive']})
 })
 
 app.get('/fire', (req,res)=>{
-    res.render('FIRE')
+    res.render('fire.ejs', {fireTraits: ['Passionate', 'Strong Emotions', 'Temperamental', 'Energetic', 'Accomplished', 'Interesting']})
 })
 
 app.get('/earth', (req,res)=>{
-    res.send('EARTH')
+    res.render('earth.ejs', {earthTraits: ['Grounded', 'Helpful', 'Practical', 'Realistic', 'Materialistic', 'Dependable']})
 })
 
 
