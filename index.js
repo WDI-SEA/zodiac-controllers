3c62dfd52a949dbe1e7f8b4212f07a67ef5298af
fjalafoo <fjalafoo@gmail.com>
2022-10-15 18:49:47 +0300

    halfway there

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //import express
-const express = require('express')
+const express = require("express")
 const PORT=3000
 
 
@@ -12,27 +12,31 @@ app.set('view engine','ejs')
 
 //HOME route
 app.get("/", (req,res)=>{
-    res.send("This is the Home Page!")
+    res.render('index.ejs')
     
 })
 
 app.get("/water", (req,res)=>{
-    res.send("This is the water sign Page!")
+    // res.send("This is the water sign Page!")
+    res.render('water.ejs')
     
 })
 
 app.get("/air", (req,res)=>{
-    res.send("This is the air sign Page!")
+    // res.send("This is the air sign Page!")
+    res.render('air.ejs')
     
 })
 
 app.get("/fire", (req,res)=>{
-    res.send("This is the fire sign Page!")
+    // res.send("This is the fire sign Page!")
+    res.render('fire.ejs')
     
 })
 
 app.get("/earth", (req,res)=>{
-    res.send("This is the earth sign Page!")
+    // res.send("This is the earth sign Page!")
+    res.render('earth.ejs')
     
 })
 
