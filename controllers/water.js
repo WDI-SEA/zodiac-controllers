f2be7963d264b1b664e0edf6f1ecfc308cc9d32a
tylersb <tylersb@gmail.com>
2022-12-15 05:42:58 -0800

    fixes hardcoded paths in controllers by importing node:path and using path.join

--- controllers/water.js
+++ controllers/water.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
 router.get('/', (req, res) => {
   res.render('water.ejs', {
@@ -16,7 +17,7 @@ router.get('/', (req, res) => {
 
 router.get('/:sign', (req, res) => {
   const sign = req.params.sign
-  res.sendFile('/home/cephandrius/seir-1114/unit2/zodiac-controllers/public/' + sign + '.png')
+  res.sendFile(path.join(__dirname, ('../public/' + sign + '.png')))
 })
 
 module.exports = router
