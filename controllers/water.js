cf11bd8595ee7a954a188fcbeb73dc58310cd237
Heiner000 <garrett.heiner@gmail.com>
2023-03-30 09:21:24 -0700

    got water controller working

--- controllers/water.js
+++ controllers/water.js
@@ -1,19 +1,27 @@
 const express = require("express")
 const router = express.Router()
 
+// water route
+router.get("/", (req, res) => {
+    res.render("water.ejs", {
+        signs: ['Cancer','Scorpio','Pisces'],
+        traits: ['private','mysterious','psychic','charming','emotional','sensitive']
+    })
+})
+
 // cancer route
 router.get("/cancer", (req, res) => {
-    res.sendFile("/cancer.ejs")
+    res.sendFile("/Users/garrettheiner/Downloads/imgs/cancer.png")
 })
 
 // scorpio route
 router.get("/scorpio", (req, res) => {
-    res.sendFile(__dirname+"/scorpio.png")
+    res.sendFile(__dirname+"/imgs/scorpio.png")
 })
 
 // pisces route
 router.get("/pisces", (req, res) => {
-    res.sendFile(__dirname+"/pisces.png")
+    res.sendFile(__dirname+"/imgs/pisces.png")
 })
 
 module.exports = router
