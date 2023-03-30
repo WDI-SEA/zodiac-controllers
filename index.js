1646d705e51e7d35c34a867c84c29562853996c8
andrewbantly <andrewbantly@gmail.com>
2023-03-29 19:40:12 -0700

    views folder boilerplate created

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ app.set("view engine", "ejs");
 
 // ROUTES 
 
+app.get("/", (req, res) => {
+    res.send("homepage is working");
+    console.log("homepage");
+})
 
+app.get("/water", (req, res) => {
+    res.send("water page is working");
+    console.log("water");
+})
+
+app.get("/air", (req, res) => {
+    res.send("air page is working");
+    console.log("air");
+})
+
+app.get("/fire", (req, res) => {
+    res.send("fire page is working");
+    console.log("fire");
+})
+
+app.get("/earth", (req, res) => {
+    res.send("earth page is working");
+    console.log("earth");
+})
 
 
 app.listen(PORT, () =>
