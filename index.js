50db483071f51121f054fc44e4c28156be3f1f3f
andrewbantly <andrewbantly@gmail.com>
2023-03-29 20:18:43 -0700

    routes linked to all views

--- index.js
+++ index.js
@@ -8,27 +8,40 @@ app.use(express.static("public"))
 // ROUTES 
 
 app.get("/", (req, res) => {
-    res.send("homepage is working");
+    // res.send("homepage is working");
+    res.render("index.ejs")
     console.log("homepage");
 })
 
 app.get("/water", (req, res) => {
-    res.send("water page is working");
+    // res.send("water page is working");
+    res.render("water.ejs", {
+        water: ['cancer', 'scorpio', 'pisces']
+    })
     console.log("water");
 })
 
 app.get("/air", (req, res) => {
-    res.send("air page is working");
+    // res.send("air page is working");
+    res.render("air.ejs", {
+        air:['gemini', 'libra', 'aquarius']
+    })
     console.log("air");
 })
 
 app.get("/fire", (req, res) => {
-    res.send("fire page is working");
+    // res.send("fire page is working");
+    res.render("fire.ejs", {
+        fire: ['leo', 'aries', 'sagittarius']
+    })
     console.log("fire");
 })
 
 app.get("/earth", (req, res) => {
-    res.send("earth page is working");
+    // res.send("earth page is working");
+    res.render("earth.ejs", {
+        earth: ['taurus', 'virgo', 'capricorn']
+    })
     console.log("earth");
 })
 
